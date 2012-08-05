e729eaed920f7a377d2b2dc82c0f5c489695d666
Yuta Kurotaki <number999666@yahoo.co.jp>
2012-08-06 06:33:48 +0900

    問5まで

--- spec/hash_quiz_spec.rb
+++ spec/hash_quiz_spec.rb
@@ -14,19 +14,19 @@ describe Hash do
   end
   
   context '1. when "heteml" exists' do
-    describe 'Hash#has_key?' do
+    describe 'Hash#has_key?("heteml")' do
       it { hash.has_key?('heteml').should be_true }
     end
 
-    describe 'Hash#key?' do
+    describe 'Hash#key?("heteml")' do
       it { hash.key?('heteml').should be_true }
     end
 
-    describe 'Hash#include?' do
+    describe 'Hash#include?("heteml")' do
       it { hash.include?('heteml').should be_true }
     end
 
-    describe 'Hash#member?' do
+    describe 'Hash#member?("heteml")' do
       it { hash.member?('heteml').should be_true }
     end
 
@@ -36,24 +36,46 @@ describe Hash do
   end
 
   context '2. when "kaba" exists' do
-    describe 'Hash#has_value?' do
+    describe 'Hash#has_value?("kaba")' do
       it { hash.has_value?('kaba').should be_true }
     end
 
-    describe 'Hash#value?' do
+    describe 'Hash#value?("kaba")' do
       it { hash.value?('kaba').should be_true }
     end
   end
 
   context '3. delete "heteml" in hash' do
-    describe 'Hash#delete' do
-      it 'delete heteml' do hash.delete('heteml')
-        hash == {"muumuu" => "kuma", "lolipop" => "ozisan"}      
+    describe 'Hash#delete?("heteml")' do
+      it 'delete heteml' do
+        hash.delete('heteml')
+        hash.should eq({"muumuu" => "kuma", "lolipop" => "ozisan"})
       end
     end
   end
 
   context '4. return the "unknwon" when access a nonexistent key' do
-  
+    describe 'Hash#default="unknown"' do
+      it 'returns "unknown"' do
+        hash.default = 'unknown'
+        hash['sqale'].should eq('unknown')
+      end
+    end
+  end
+
+  context '5. returns an arrays with all keys of the hash' do
+    describe 'Hash#keys' do
+      it 'returns an arrays' do
+        hash.keys.should eq(["heteml", "muumuu", "lolipop"])
+      end
+    end
+  end
+
+  context '6. output iterator' do
+    describe 'Hash#each' do
+      it 'output iterator' do
+        hash.each_key { |key| puts "#{key}"}
+      end
+    end
   end
 end
