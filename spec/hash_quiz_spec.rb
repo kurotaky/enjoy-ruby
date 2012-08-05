3dd7264727fe9fb96b4e5aa16ded04c1581f123c
Yuta Kurotaki <number999666@yahoo.co.jp>
2012-08-05 19:11:54 +0900

    問1おわり

--- spec/hash_quiz_spec.rb
+++ spec/hash_quiz_spec.rb
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+
+# RSpecの書き方参考　
+# http://tech.recompile.net/post/21340599029/rspec
+# rspec -fd -c でドキュメンテーションフォーマット+色付を指定
+
 describe Hash do
   let(:hash) do
     {
@@ -7,8 +12,39 @@ describe Hash do
       "lolipop" => "ozisan",
     }
   end
+  
+  context 'when "heteml" exists' do
+    describe 'Hash#has_key?' do
+      it 'returns true' do
+        hash.has_key?('heteml').should be_true
+      end
+    end
 
-  it "キー heteml が存在するか確認する"do
-    hash.has_key?("heteml").should == true
+    describe 'Hash#key?' do
+      it 'returns true' do
+        hash.key?('heteml').should be_true
+      end
+    end
+
+    describe 'Hash#include?' do
+      it 'returns true' do
+        hash.include?('heteml').should be_true
+      end
+    end
+
+    describe 'Hash#member?' do
+      it 'returns true' do
+        hash.member?('heteml').should be_true
+      end
+    end
+    
+    describe 'Hash["heteml"]' do
+      it 'returns true' do
+        hash["heteml"].should be_true
+      end
+    end
+  end
+
+  context 'when "kaba" exists' do
   end
 end
