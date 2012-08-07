5ccb585418656632d9942eeabe4b9de908be9401
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-08-07 09:17:34 +0900

    11まで終了

--- spec/hash_quiz_spec.rb
+++ spec/hash_quiz_spec.rb
@@ -100,7 +100,7 @@ describe Hash do
         hash.each_key { |key| puts "#{key}"}
         out = $stdout.string
         $stdout = STDOUT
-        out.should == "heteml\nmuumuu\nlolipop\n"
+        out.should eq("heteml\nmuumuu\nlolipop\n")
       end
     end
   end
@@ -120,7 +120,7 @@ describe Hash do
         hash.each_value { |value| puts "#{value}"}
         out = $stdout.string
         $stdout = STDOUT
-        out.should == "kaba\nkuma\nozisan\n"
+        out.should eq("kaba\nkuma\nozisan\n")
       end
     end
   end
@@ -132,7 +132,7 @@ describe Hash do
         hash.each { |key, value| puts "#{key} #{value}"}
         out = $stdout.string
         $stdout = STDOUT
-        out.should == "heteml kaba\nmuumuu kuma\nlolipop ozisan\n"
+        out.should eq("heteml kaba\nmuumuu kuma\nlolipop ozisan\n")
       end
     end
 
@@ -142,41 +142,95 @@ describe Hash do
         hash.each_pair { |key, value| puts "#{key} #{value}"}
         out = $stdout.string
         $stdout = STDOUT
-        out.should == "heteml kaba\nmuumuu kuma\nlolipop ozisan\n"
+        out.should eq("heteml kaba\nmuumuu kuma\nlolipop ozisan\n")
       end
     end
   end
 
   context  '10. combine hash with another_hash' do
+    let(:merge_hash) do
+      { "heteml"=>"kaba", "muumuu"=>"kuma", "lolipop"=>"ozisan", 
+        "hatena"=>"cinnamon", "oracle"=>"candy" }
+    end
+    
     describe 'Hash#merge' do
       it 'combine hash & another_hash' do
-        hash.merge(another_hash).should eq({"heteml"=>"kaba",
-                                             "muumuu"=>"kuma",
-                                             "lolipop"=>"ozisan",
-                                             "hatena"=>"cinnamon",
-                                             "oracle"=>"candy"})
+        hash.merge(another_hash).should eq(merge_hash)
       end
     end
 
 
     describe 'Hash#update' do
       it 'combine hash & another_hash' do
-        hash.update(another_hash).should eq({"heteml"=>"kaba",
-                                             "muumuu"=>"kuma",
-                                             "lolipop"=>"ozisan",
-                                             "hatena"=>"cinnamon",
-                                             "oracle"=>"candy"})
+        hash.update(another_hash).should eq(merge_hash)
       end
     end
   end
 
-  pending '11. Access to complex structures' do
+  context '11. Access to complex structures' do
     describe 'Hash#merge' do
-      it 'combine hash & another_hash' do
-     
+      it 'returns app001.sqale.jp' do
+        sqale["production"]["app"][0].should eq("app001.sqale.jp")
+      end
+
+      it 'returns users002.sqale.jp' do
+        sqale["production"]["users"][1].should eq("users002.sqale.jp")
       end
     end
   end
-
-
 end
+
+# rspec spec/ -fd -c
+#
+# Hash
+#   1. when "heteml" exists
+#     Hash#has_key?("heteml")
+#       should be true
+#     Hash#key?("heteml")
+#       should be true
+#     Hash#include?("heteml")
+#       should be true
+#     Hash#member?("heteml")
+#       should be true
+#     Hash["heteml"]
+#       should be true
+#   2. when "kaba" exists
+#     Hash#has_value?("kaba")
+#       should be true
+#     Hash#value?("kaba")
+#       should be true
+#   3. delete "heteml" in hash
+#     Hash#delete?("heteml")
+#       delete heteml
+#   4. return the "unknwon" when access a nonexistent key
+#     Hash#default="unknown"
+#       returns "unknown"
+#   5. returns an arrays with all keys of the hash
+#     Hash#keys
+#       returns an arrays
+#   6. hash key output iterator
+#     Hash#each_key
+#       output iterator
+#   7. returns an arrays with all value of the hash
+#     Hash#values
+#       returns an arrays
+#   8. hash value output iterator
+#     Hash#each_value
+#       output iterator
+#   9. hash value & key output iterator
+#     Hash#each
+#       output iterator
+#     Hash#each_pair
+#       output iterator
+#   10. combine hash with another_hash
+#     Hash#merge
+#       combine hash & another_hash
+#     Hash#update
+#       combine hash & another_hash
+#   11. Access to complex structures
+#     Hash#merge
+#       returns app001.sqale.jp
+#       returns users002.sqale.jp
+
+# Finished in 0.00541 seconds
+# 19 examples, 0 failures
